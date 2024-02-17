9fc18e9106f5a7cc108e876b60fd5380033f2f37
LuisMiguelTrinidad <lmtrinidadsalvador@gmail.com>
2024-02-17 14:04:09 +0100

    Commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("hola")
-    document.querySelectorAll('.accordeonhead').forEach(i => {
+    document.querySelectorAll('.accordeon').forEach(i => {
         console.log(i)
         i.firstElementChild.addEventListener("click", j => {
             if(i.lastElementChild.style.display!="grid"){
@@ -8,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 i.firstElementChild.firstElementChild.textContent = "expand_less"
             } else {
                 i.lastElementChild.style.display = "none";
+                i.firstElementChild.firstElementChild.style.transition = "0.5s"
+                i.firstElementChild.firstElementChild.style.transition = "0.5s"
                 i.firstElementChild.firstElementChild.textContent = "expand_more"
             }
         })
