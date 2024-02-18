9a30639d37b5df3765cce67c093f925db06adb45
LuisMiguelTrinidad <lmtrinidadsalvador@gmail.com>
2024-02-19 00:03:14 +0100

    Falta 50 % responsividad, cookies y busqueda

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 i.firstElementChild.firstElementChild.textContent = "expand_less"
             } else {
                 i.lastElementChild.style.display = "none";
-                i.firstElementChild.firstElementChild.style.transition = "0.5s"
-                i.firstElementChild.firstElementChild.style.transition = "0.5s"
                 i.firstElementChild.firstElementChild.textContent = "expand_more"
             }
         })
@@ -15,81 +13,94 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.querySelectorAll(".paginasleidas").forEach(i => {
 
-        //Para no poder poner mas paginas de las que hay en total
-        if(Number(i.value) > Number(i.parentElement.children[2].value)){
-            i.value = i.parentElement.children[2].value;
-        }
-        let paginastotalescosmere = Array.from(document.querySelectorAll(".paginastotales"))
-            .reduce((x, y) => x + Number(y.value), 0);
-        let paginasleidascosmere = Array.from(document.querySelectorAll(".paginasleidas"))
-            .reduce((x, y) => x + Number(y.value), 0);
-        let barracosmereinterior = document.getElementById("progresototaldentro");
-        let barracosmereexterior = document.getElementById("progresototalfuera");
-        
-        document.getElementById("porcleido").innerHTML=`${Math.floor(1000*paginasleidascosmere/paginastotalescosmere)/10}%`;
-        barracosmereinterior.style.width=`${Math.max(barracosmereexterior.offsetWidth * paginasleidascosmere/paginastotalescosmere, 36)}px`;
-        barracosmereinterior.innerHTML=`<span id="resultado">${paginasleidascosmere}/${paginastotalescosmere}</span>`;
-        
-        //Paginas leidas por saga
-        let paginastotalessaga = Array.from(i.parentElement.parentElement.parentElement
-            .querySelectorAll(".paginastotales"))
-            .reduce((x, y) => x + Number(y.value), 0);
-        let paginasleidassaga = Array.from(i.parentElement.parentElement.parentElement
-            .querySelectorAll(".paginasleidas"))
-            .reduce((x, y) => x + Number(y.value), 0);
-        let barrasagainterior = i.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0];
-        let barrasagaexterior = i.parentElement.parentElement.parentElement.parentElement.nextElementSibling
-            
-        barrasagainterior.style.width=`${Math.max(barrasagaexterior.offsetWidth * paginasleidassaga/paginastotalessaga, 36)}px`;
-        barrasagainterior.innerHTML=`<span id="resultado">${paginasleidassaga}/${paginastotalessaga}</span>`;
-                    
-        //Paginas leidas por libro
-        let paginasleidaslibro = i.value;
-        let paginastotaleslibro = i.parentElement.children[2].value;
-        let barralibrointerior =  i.parentElement.nextElementSibling.children[0]
-        let barralibroexterior =  i.parentElement.nextElementSibling
-        barralibrointerior.style.width=`${Math.max(barralibroexterior.offsetWidth * paginasleidaslibro/paginastotaleslibro, 24)}px`;
+		//Para no poder poner mas paginas de las que hay en total
+		if(Number(i.value) > Number(i.parentElement.children[2].value)){
+			i.value = i.parentElement.children[2].value;
+		}
+		
+		generabarrasaga();
+		generabarralibro();
+		generabarracosmere();
 
+		function generabarracosmere(){
+			let paginastotalescosmere = Array.from(document.querySelectorAll(".paginastotales"))
+            	.reduce((x, y) => x + Number(y.value), 0);
+        	let paginasleidascosmere = Array.from(document.querySelectorAll(".paginasleidas"))
+				.reduce((x, y) => x + Number(y.value), 0);
 
+			let barracosmereinterior = document.createElement("div");
+			barracosmereinterior.id="progresototaldentro";
+			let barracosmereexterior = document.getElementById("progresototalfuera");
+
+			let spancosmere = document.createElement("span");
+			spancosmere.classList  = "leidoytotal"
+			spancosmere.innerHTML = `${paginasleidascosmere}/${paginastotalescosmere}`;
+			
+			barracosmereexterior.innerHTML = "";			
+			barracosmereexterior.append(barracosmereinterior)
+			barracosmereexterior.append(spancosmere);			
+			
+			document.getElementById("porcleido").innerHTML=`${Math.floor(
+				1000*paginasleidascosmere/paginastotalescosmere
+			)/10}%`;
+			barracosmereinterior.style.width=`${Math.min(
+				barracosmereexterior.offsetWidth * (1 - paginasleidascosmere/paginastotalescosmere), 
+				barracosmereexterior.offsetWidth - 24)
+			}px`;
+		}
+
+		function generabarrasaga(){
+			let paginastotalessaga = Array.from(i.parentElement.parentElement.parentElement
+				.querySelectorAll(".paginastotales"))
+				.reduce((x, y) => x + Number(y.value), 0);
+			let paginasleidassaga = Array.from(i.parentElement.parentElement.parentElement
+				.querySelectorAll(".paginasleidas"))
+				.reduce((x, y) => x + Number(y.value), 0);
+
+			let barrasagaexterior = i.parentElement.parentElement.parentElement.parentElement.nextElementSibling
+			let barrasagainterior = document.createElement("div");
+			barrasagainterior.classList="progresosagadentro";
+			
+			let spansaga = document.createElement("span");
+			spansaga.classList  = "leidoytotal"
+			spansaga.innerHTML = `${paginasleidassaga}/${paginastotalessaga}`;
+			
+			barrasagaexterior.innerHTML = "";
+			barrasagaexterior.append(barrasagainterior)
+			barrasagaexterior.append(spansaga);
+
+			barrasagainterior.style.width=`${Math.min(
+				barrasagaexterior.offsetWidth * (1 - paginasleidassaga/paginastotalessaga), 
+				barrasagaexterior.offsetWidth - 24)
+			}px`;
+		}
+
+		function generabarralibro(){
+			//Paginas leidas por libro
+			let paginasleidaslibro = i.value;
+			let paginastotaleslibro = i.parentElement.children[2].value;
+			let barralibrointerior =  i.parentElement.nextElementSibling.children[0]
+			let barralibroexterior =  i.parentElement.nextElementSibling
+			barralibrointerior.style.width=`${Math.max(
+				barralibroexterior.offsetWidth * paginasleidaslibro/paginastotaleslibro - 3, 24
+			)}px`;
+		}
             
         i.addEventListener("blur", j => {
             //Para no poder poner mas paginas de las que hay en total
-            if(Number(j.target.value) > Number(j.target.parentElement.children[2].value)){
-                console.log("Hola")
-                j.target.value = j.target.parentElement.children[2].value;
+            if(Number(i.value) > Number(i.parentElement.children[2].value)){
+                i.value = i.parentElement.children[2].value;
             }
-            //Paginas leidas totales
-            let paginastotalescosmere = Array.from(document.querySelectorAll(".paginastotales"))
-                .reduce((x, y) => x + Number(y.value), 0);
-            let paginasleidascosmere = Array.from(document.querySelectorAll(".paginasleidas"))
-                .reduce((x, y) => x + Number(y.value), 0);
-            let barracosmereinterior = document.getElementById("progresototaldentro");
-            let barracosmereexterior = document.getElementById("progresototalfuera");
-            
-            document.getElementById("porcleido").innerHTML=`${Math.floor(1000*paginasleidascosmere/paginastotalescosmere)/10}%`;
-            barracosmereinterior.style.width=`${Math.max(barracosmereexterior.offsetWidth * paginasleidascosmere/paginastotalescosmere, 36)}px`;
-            barracosmereinterior.innerHTML=`<span id="resultado">${paginasleidascosmere}/${paginastotalescosmere}</span>`;
-
-            //Paginas leidas por saga
-            let paginastotalessaga = Array.from(j.target.parentElement.parentElement.parentElement
-                .querySelectorAll(".paginastotales"))
-                .reduce((x, y) => x + Number(y.value), 0);
-            let paginasleidassaga = Array.from(j.target.parentElement.parentElement.parentElement
-                .querySelectorAll(".paginasleidas"))
-                .reduce((x, y) => x + Number(y.value), 0);
-            let barrasagainterior = j.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0];
-            let barrasagaexterior = j.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling
-                
-            barrasagainterior.style.width=`${Math.max(barrasagaexterior.offsetWidth * paginasleidassaga/paginastotalessaga, 36)}px`;
-            barrasagainterior.innerHTML=`<span id="resultado">${paginasleidassaga}/${paginastotalessaga}</span>`;
-            
-            //Paginas leidas por libro
-            let paginasleidaslibro =j.target.value;
-            let paginastotaleslibro = j.target.parentElement.children[2].value;
-            let barralibrointerior =  j.target.parentElement.nextElementSibling.children[0]
-            let barralibroexterior =  j.target.parentElement.nextElementSibling
-            barralibrointerior.style.width=`${Math.max(barralibroexterior.offsetWidth * paginasleidaslibro/paginastotaleslibro, 24)}px`;
+			console.log()
+			if(isNaN(Number(i.value))){
+                i.value = 0;
+            }
+			generabarrasaga();
+			generabarralibro();
+			generabarracosmere();
         })
     })
+	
+
 });
 
