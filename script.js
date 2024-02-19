41326afebb2db9fe7c240444a9abd74674050572
LuisMiguelTrinidad <lmtrinidadsalvador@gmail.com>
2024-02-19 13:41:09 +0100

    Cookies funcionando, hay que refactorizar código

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ document.addEventListener("DOMContentLoaded", function() {
 		//Para no poder poner mas paginas de las que hay en total
 		if(Number(i.value) > Number(i.parentElement.children[2].value)){
 			i.value = i.parentElement.children[2].value;
-		}
-		
+		}		
+
+		adjudicaCookies();
 		generabarrasaga();
 		generabarralibro();
 		generabarracosmere();
@@ -86,6 +87,40 @@ document.addEventListener("DOMContentLoaded", function() {
 			)}px`;
 		}
 
+		function generacookie(){
+			let nombreInput = i.parentElement.previousElementSibling.previousElementSibling.textContent
+			let leido = i.value
+			let actual = i.parentElement.children[2].value;
+			document.cookie = nombreInput + "=" + leido + "/" + actual + "; expires=Thu, 31 Dec 2026 12:00:00 UTC; path=/";
+		}
+
+		function getCookie(nombre) {
+			// Obtener todas las cookies del documento
+			var cookies = document.cookie.split(';');
+			
+			// Iterar sobre las cookies para encontrar la que coincide con el nombre
+			for (var i = 0; i < cookies.length; i++) {
+				var cookie = cookies[i];
+				// Eliminar espacios en blanco al principio y al final de la cookie
+				while (cookie.charAt(0) === ' ') {
+					cookie = cookie.substring(1);
+				}
+				// Verificar si la cookie comienza con el nombre buscado
+				if (cookie.indexOf(nombre + '=') === 0) {
+					// Devolver el valor de la cookie
+					return cookie.substring(nombre.length + 1, cookie.length);
+				}
+			}
+			// Si no se encuentra la cookie, devolver null
+			return "";
+		}
+
+		function adjudicaCookies(){
+			let leidasbarratotal = getCookie(i.parentElement.previousElementSibling.previousElementSibling.textContent);
+			i.value = leidasbarratotal===""?0:leidasbarratotal.split("/")[0];
+			i.parentElement.children[2].value = leidasbarratotal===""?i.parentElement.children[2].value:leidasbarratotal.split("/")[1];
+		}
+
 		let timer;
             
 		function startTimer() {
@@ -99,6 +134,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				generabarrasaga();
 				generabarralibro();
 				generabarracosmere();
+				generacookie();
 			}, 500);
 		}
 		function resetTimer() {
@@ -118,19 +154,60 @@ document.addEventListener("DOMContentLoaded", function() {
 			}
 		});
 
-        i.addEventListener("blur", j => {
-            //Para no poder poner mas paginas de las que hay en total
-            if(Number(i.value) > Number(i.parentElement.children[2].value)){
-                i.value = i.parentElement.children[2].value;
-            }
-			console.log()
-			if(isNaN(Number(i.value))){
-                i.value = 0;
-            }
+		function handleBlur(event) {
+			const input = event.target;
+			const totalPages = Number(input.parentElement.children[2].value);
+		
+			if (Number(input.value) > totalPages) {
+				input.value = totalPages;
+			}
+		
+			if (isNaN(Number(input.value))) {
+				input.value = 0;
+			}
+		
 			generabarrasaga();
 			generabarralibro();
 			generabarracosmere();
-        })
-    })
+			generacookie();
+		}
+		
+		i.addEventListener("blur", handleBlur);
+		i.parentElement.children[2].addEventListener("blur", handleBlur);		
+    });
+	document.querySelectorAll(".paginastotales").forEach(i => {
+		let timer;
+            
+		function startTimer() {
+			timer = setTimeout(() => {
+				if (Number(i.parentElement.children[0].value) > Number(i.value)) {
+					i.parentElement.children[0].value = i.value;
+				}
+				if (isNaN(Number(i.value))) {
+					i.value = 0;
+				}
+				generabarrasaga();
+				generabarralibro();
+				generabarracosmere();
+				generacookie();
+			}, 500);
+		}
+		function resetTimer() {
+			clearTimeout(timer);
+		}
+
+		i.addEventListener("keydown", function(event) {
+			if (event.key !== " ") {
+				if (!timer) {
+					startTimer();
+				} else {
+					resetTimer();
+					startTimer(); 
+				}
+			} else {
+				resetTimer(); 
+			}
+		});
+	});
 });
 
