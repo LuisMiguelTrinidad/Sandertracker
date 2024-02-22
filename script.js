f9312227a4ad986d1687139f032a75639ea2d8cd
LuisMiguelTrinidad <lmtrinidadsalvador@gmail.com>
2024-02-22 12:40:24 +0100

    Todo funcionando

--- script.js
+++ script.js
@@ -4,133 +4,27 @@ document.addEventListener("DOMContentLoaded", function() {
             if(i.lastElementChild.style.display!="grid"){
                 i.lastElementChild.style.display = "grid";
                 i.firstElementChild.firstElementChild.textContent = "expand_less"
+				//Genera las barras de progreso de cada libro
+				i.querySelectorAll(".paginasleidas").forEach(k => generabarralibro(k))
+				
             } else {
                 i.lastElementChild.style.display = "none";
                 i.firstElementChild.firstElementChild.textContent = "expand_more"
             }
-        })
+        })	
+		generabarrasaga(i.children[1]);	
     })
 
+	//Carga los valores guardados en cookies
 	document.querySelectorAll(".paginasleidas").forEach(i => {
-		adjudicaCookies();
-
-		function getCookie(nombre) {
-			// Obtener todas las cookies del documento
-			var cookies = document.cookie.split(';');
-			
-			// Iterar sobre las cookies para encontrar la que coincide con el nombre
-			for (var i = 0; i < cookies.length; i++) {
-				var cookie = cookies[i];
-				// Eliminar espacios en blanco al principio y al final de la cookie
-				while (cookie.charAt(0) === ' ') {
-					cookie = cookie.substring(1);
-				}
-				// Verificar si la cookie comienza con el nombre buscado
-				if (cookie.indexOf(nombre + '=') === 0) {
-					// Devolver el valor de la cookie
-					return cookie.substring(nombre.length + 1, cookie.length);
-				}
-			}
-			// Si no se encuentra la cookie, devolver null
-			return "";
-		}
-
-		function adjudicaCookies(){
-			let leidasbarratotal = getCookie(i.parentElement.previousElementSibling.previousElementSibling.textContent);
-			i.value = leidasbarratotal===""?0:leidasbarratotal.split("/")[0];
-			i.parentElement.children[2].value = leidasbarratotal===""?i.parentElement.children[2].value:leidasbarratotal.split("/")[1];
-		}
+		adjudicaCookies(i);
 	});
 
-    document.querySelectorAll(".paginasleidas").forEach(i => {
+	//Generamos la barra total
+	generabarracosmere();
 
-		//Para no poder poner mas paginas de las que hay en total
-		if(Number(i.value) > Number(i.parentElement.children[2].value)){
-			i.value = i.parentElement.children[2].value;
-		}		
-
-		function generacookie(){
-			let nombreInput = i.parentElement.previousElementSibling.previousElementSibling.textContent
-			let leido = i.value
-			let actual = i.parentElement.children[2].value;
-			document.cookie = nombreInput + "=" + leido + "/" + actual + "; expires=Thu, 31 Dec 2026 12:00:00 UTC; path=/";
-		}
-		generabarralibro();
-		generabarrasaga();
-		generabarracosmere();
-
-		function generabarracosmere(){
-			let paginastotalescosmere = Array.from(document.querySelectorAll(".paginastotales"))
-            	.reduce((x, y) => x + Number(y.value), 0);
-        	let paginasleidascosmere = Array.from(document.querySelectorAll(".paginasleidas"))
-				.reduce((x, y) => x + Number(y.value), 0);
-
-			let barracosmereinterior = document.createElement("div");
-			barracosmereinterior.id="progresototaldentro";
-			let barracosmereexterior = document.getElementById("progresototalfuera");
-
-			let spancosmere = document.createElement("span");
-			spancosmere.classList  = "leidoytotal"
-			spancosmere.innerHTML = `${paginasleidascosmere}/${paginastotalescosmere}`;
-			
-			barracosmereexterior.innerHTML = "";			
-			barracosmereexterior.append(barracosmereinterior)
-			barracosmereexterior.append(spancosmere);			
-			
-			document.getElementById("porcleido").innerHTML=`${Math.floor(
-				1000*paginasleidascosmere/paginastotalescosmere
-			)/10}%`;
-			barracosmereinterior.style.width=`${Math.min(
-				barracosmereexterior.offsetWidth * (1 - paginasleidascosmere/paginastotalescosmere), 
-				barracosmereexterior.offsetWidth - 24)
-			}px`;
-		}
-
-		function generabarrasaga(){
-			let paginastotalessaga = Array.from(i.parentElement.parentElement.parentElement
-				.querySelectorAll(".paginastotales"))
-				.reduce((x, y) => x + Number(y.value), 0);
-			let paginasleidassaga = Array.from(i.parentElement.parentElement.parentElement
-				.querySelectorAll(".paginasleidas"))
-				.reduce((x, y) => x + Number(y.value), 0);
-
-			let barrasagaexterior = i.parentElement.parentElement.parentElement.parentElement.nextElementSibling
-			let barrasagainterior = document.createElement("div");
-			barrasagainterior.classList="progresosagadentro";
-			
-			let spansaga = document.createElement("span");
-			spansaga.classList  = "leidoytotal"
-			spansaga.innerHTML = `${paginasleidassaga}/${paginastotalessaga}`;
-			
-			barrasagaexterior.innerHTML = "";
-			barrasagaexterior.append(barrasagainterior)
-			barrasagaexterior.append(spansaga);
-
-			barrasagainterior.style.width=`${Math.min(
-				barrasagaexterior.offsetWidth * (1 - paginasleidassaga/paginastotalessaga), 
-				barrasagaexterior.offsetWidth - 24)
-			}px`;
-		}
-
-		function generabarralibro(){
-			//Paginas leidas por libro
-			let paginasleidaslibro = i.value;
-			let paginastotaleslibro = i.parentElement.children[2].value;
-			let barralibrointerior =  i.parentElement.nextElementSibling.children[0];
-			let porcentajeinicial;
-			if(window.screen.width < 550){
-				porcentajeinicial = 12.5;
-			} else if (window.screen.width < 950){
-				porcentajeinicial = 10;
-			} else {
-				porcentajeinicial = 7.5;
-			}
-			barralibrointerior.style.width=`${Math.max(
-				100 * paginasleidaslibro/paginastotaleslibro, porcentajeinicial
-			)}%`;
-		}
-
-
+	//Funciones y event listeners para actualizar los datos de paginas leidas
+    document.querySelectorAll(".paginasleidas").forEach(i => {	
 
 		let timer;
             
@@ -142,10 +36,10 @@ document.addEventListener("DOMContentLoaded", function() {
 				if (isNaN(Number(i.value))) {
 					i.value = 0;
 				}
-				generabarrasaga();
-				generabarralibro();
+				generabarralibro(i);
+				generabarrasaga(i.parentElement.parentElement.parentElement);	
 				generabarracosmere();
-				generacookie();
+				generacookie(i.parentElement.previousElementSibling.previousElementSibling, i, i.parentElement.children[2]);
 			}, 500);
 		}
 		function resetTimer() {
@@ -177,10 +71,10 @@ document.addEventListener("DOMContentLoaded", function() {
 				input.value = 0;
 			}
 		
-			generabarrasaga();
-			generabarralibro();
+			generabarrasaga(i.parentElement.parentElement.parentElement);
+			generabarralibro(i);
 			generabarracosmere();
-			generacookie();
+			generacookie(i.parentElement.previousElementSibling.previousElementSibling, i, i.parentElement.children[2]);
 		}
 		
 		i.addEventListener("blur", handleBlur);
@@ -197,10 +91,10 @@ document.addEventListener("DOMContentLoaded", function() {
 				if (isNaN(Number(i.value))) {
 					i.value = 0;
 				}
-				generabarrasaga();
-				generabarralibro();
+				generabarrasaga(i.parentElement.parentElement.parentElement);
+				generabarralibro(i);
 				generabarracosmere();
-				generacookie();
+				generacookie(i.parentElement.previousElementSibling.previousElementSibling, i, i.parentElement.children[2]);
 			}, 500);
 		}
 		function resetTimer() {
@@ -220,5 +114,103 @@ document.addEventListener("DOMContentLoaded", function() {
 			}
 		});
 	});
+
+	//Funciones para generar barras
+	function generabarralibro(elemento){
+		//Paginas leidas por libro
+		let paginasleidaslibro = elemento.value;
+		let paginastotaleslibro = elemento.parentElement.children[2].value;
+		let barralibrointerior =  elemento.parentElement.nextElementSibling.children[0];
+		let barralibroexterior = elemento.parentElement.nextElementSibling;
+		barralibrointerior.style.width=`${Math.max(
+			paginasleidaslibro/paginastotaleslibro, 24/barralibroexterior.offsetWidth
+		) * 100}%`;
+	}
+
+	function generabarrasaga(elemento){
+		let paginastotalessaga = Array.from(elemento.querySelectorAll(".paginastotales"))
+			.reduce((x, y) => x + Number(y.value), 0);
+		let paginasleidassaga = Array.from(elemento.querySelectorAll(".paginasleidas"))
+			.reduce((x, y) => x + Number(y.value), 0);
+
+		let barrasagaexterior = elemento.parentElement.nextElementSibling
+		let barrasagainterior = document.createElement("div");
+		barrasagainterior.classList="progresosagadentro";
+		
+		let spansaga = document.createElement("span");
+		spansaga.classList  = "leidoytotal"
+		spansaga.innerHTML = `${paginasleidassaga}/${paginastotalessaga}`;
+		
+		barrasagaexterior.innerHTML = "";
+		barrasagaexterior.append(barrasagainterior)
+		barrasagaexterior.append(spansaga);
+
+		barrasagainterior.style.width=`${Math.min(
+			barrasagaexterior.offsetWidth * (1 - paginasleidassaga/paginastotalessaga), 
+			barrasagaexterior.offsetWidth - 24)
+		}px`;
+	}
+
+	function generabarracosmere(){
+		let paginastotalescosmere = Array.from(document.querySelectorAll(".paginastotales"))
+			.reduce((x, y) => x + Number(y.value), 0);
+		let paginasleidascosmere = Array.from(document.querySelectorAll(".paginasleidas"))
+			.reduce((x, y) => x + Number(y.value), 0);
+
+		let barracosmereinterior = document.createElement("div");
+		barracosmereinterior.id="progresototaldentro";
+		let barracosmereexterior = document.getElementById("progresototalfuera");
+
+		let spancosmere = document.createElement("span");
+		spancosmere.classList  = "leidoytotal"
+		spancosmere.innerHTML = `${paginasleidascosmere}/${paginastotalescosmere}`;
+		
+		barracosmereexterior.innerHTML = "";			
+		barracosmereexterior.append(barracosmereinterior)
+		barracosmereexterior.append(spancosmere);			
+		
+		document.getElementById("porcleido").innerHTML=`${Math.floor(
+			1000*paginasleidascosmere/paginastotalescosmere
+		)/10}%`;
+		barracosmereinterior.style.width=`${Math.min(
+			barracosmereexterior.offsetWidth * (1 - paginasleidascosmere/paginastotalescosmere), 
+			barracosmereexterior.offsetWidth - 24)
+		}px`;
+	}
+
+	//Funciones para generar y usar cookies
+	function generacookie(nombreCookie, pagleidas, pagactuales){
+		let nombreInput = nombreCookie.textContent
+		let leido = pagleidas.value
+		let actual = pagactuales.value;
+		document.cookie = nombreInput + "=" + leido + "/" + actual + "; expires=Thu, 31 Dec 2026 12:00:00 UTC; path=/";
+	}
+
+	function getCookie(nombre) {
+		// Obtener todas las cookies del documento
+		var cookies = document.cookie.split(';');
+		
+		// Iterar sobre las cookies para encontrar la que coincide con el nombre
+		for (var j = 0; j < cookies.length; j++) {
+			var cookie = cookies[j];
+			// Eliminar espacios en blanco al principio y al final de la cookie
+			while (cookie.charAt(0) === ' ') {
+				cookie = cookie.substring(1);
+			}
+			// Verificar si la cookie comienza con el nombre buscado
+			if (cookie.indexOf(nombre + '=') === 0) {
+				// Devolver el valor de la cookie
+				return cookie.substring(nombre.length + 1, cookie.length);
+			}
+		}
+		// Si no se encuentra la cookie, devolver null
+		return "";
+	}
+
+	function adjudicaCookies(elemento){
+		let leidasbarratotal = getCookie(elemento.parentElement.previousElementSibling.previousElementSibling.textContent);
+		elemento.value = leidasbarratotal===""?0:leidasbarratotal.split("/")[0];
+		elemento.parentElement.children[2].value = leidasbarratotal===""?elemento.parentElement.children[2].value:leidasbarratotal.split("/")[1];
+	}
 });
 
